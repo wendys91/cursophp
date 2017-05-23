34c2609742a65b5aaf4deeabb2d5d1521f75f97d
wendolin <wendolinburciaga@91gmail.com>
2017-05-23 03:20:24 -0600

    Tema 1 Modulo1

---
+++ Modulo1/sintaxis.php
@@ -0,0 +1,27 @@
+<?php
+  // PHP code goes here
+?>
+<html>
+  <head>
+    <title>My First PHP Page</title>
+  </head>
+  <body>
+  <?php   
+    echo "Hello World!";
+  ?>
+  </body>
+</html>
+
+<html>
+  <head>
+    <title>My First PHP Page</title>
+  </head>
+  <body>
+  <script language="php">
+    echo "Hello World!";
+  </script>
+  </body>
+</html>
+<?
+  echo "Hello World!";
+?>
